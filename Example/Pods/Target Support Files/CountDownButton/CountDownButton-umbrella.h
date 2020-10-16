1dd248203f4c381805c7e269ad1d9a47ff17fb96
kouyunpeng <kyp5146@126.com>
2020-10-16 15:44:27 +0800

    Init

---
+++ Example/Pods/Target Support Files/CountDownButton/CountDownButton-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "GHCountdownButton.h"
+#import "GHImageByColor.h"
+#import "GHMacros.h"
+#import "NSTimer+GHExtend.h"
+
+FOUNDATION_EXPORT double CountDownButtonVersionNumber;
+FOUNDATION_EXPORT const unsigned char CountDownButtonVersionString[];
+
